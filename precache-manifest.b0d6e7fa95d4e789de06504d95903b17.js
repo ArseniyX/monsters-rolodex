f608a668a72f7e41cbb6d1ce4c6c9552244a1391
ArseniyX <jkl@usa.com>
2020-04-14 08:21:27 +0300

    Updates

---
+++ precache-manifest.b0d6e7fa95d4e789de06504d95903b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93853a8a1e164dbbdef90ba208c75306",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "a4232cedfa3cf191b00a",
+    "url": "/monsters-roledex/static/css/main.65ca863b.chunk.css"
+  },
+  {
+    "revision": "d97e3ef4dcc9b9be121f",
+    "url": "/monsters-roledex/static/js/2.09b1e3db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.09b1e3db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4232cedfa3cf191b00a",
+    "url": "/monsters-roledex/static/js/main.2151bd3e.chunk.js"
+  },
+  {
+    "revision": "c71d1b78de2cf468472c",
+    "url": "/monsters-roledex/static/js/runtime-main.e2c3eb12.js"
+  }
+]);
